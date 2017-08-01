818f812fa657cdc789d7736227527dac5d3571c1
Surlent <24030275@qq.com>
2017-08-01 16:18:27 +0800

    改了不少

--- loaddata.py
+++ loaddata.py
@@ -1,41 +1,47 @@
-# -*- coding:utf8 -*-
-
 import xlrd
 import xlwt
 import xlutils
 import time
 
 
-rawMonthSTR = input('Please Input Last Month :\n格式为20xx0x\n')  # 数据月份
+rawMonthSTR = "201706"  #  input('Please Input Last Month :\n格式为20xx0x\n')  # 数据月份
 rawMonthTS = time.strptime(rawMonthSTR,"%Y%m")  # 转换
 # print(rawMonthTS)
 fileName = r"E:/每月报表/" + rawMonthSTR + "test/" + rawMonthSTR + "销售清单.xlsx"  # test注意这是一个测试脚本
 rawMaterailBook = xlrd.open_workbook(fileName)
 sheet_name = rawMaterailBook.sheet_names()[0]
-print(sheet_name)
+#  print(sheet_name)
 sheet = rawMaterailBook.sheet_by_index(0)
 
 
 class invoice(object):
 
-    def __init__(self,number,custom,product,spec,weight,price,trademethed,packingfee):
+    def __init__(self,number,serialnumber,custom,product,weight,price,trademethod):
         self.number = number
+        self.serialnumber = serialnumber
         self.custom = custom
         self.product = product
-        self.spec = spec
         self.weight = weight
         self.price = price
-        self.trademethed = trademethed
-        self.packingfee = packingfee
+        self.trademethod = trademethod
+        #  self.packingfee = packingfee
 
-invoiceList=()
-# for rows in range(sheet.nrows):
 
-row_data = sheet.row_values(0)  # 获得第1行的数据列表
-print(row_data)
-col_data = sheet.col_values(0)  # 获得第一列的数据列表，然后就可以迭代里面的数据了
-print(col_data)
-cell_value1 = sheet.cell_value(0, 0)  # 只有cell的值内容
-print(cell_value1)
-cell_value2 = sheet.cell(0, 0)  # 除了cell值内容外还有附加属性
-print(cell_value2)
+print(sheet.cell(2, 4).value)
+
+invoiceList=[]
+for row in range(1,4):
+    invoiceList[row]= invoice(
+    sheet.cell(row, 0).value,
+    sheet.cell(row, 1).value,
+    sheet.cell(row, 3).value,
+    sheet.cell(row, 4).value,
+    sheet.cell(row, 5).value,
+    sheet.cell(row, 6).value,
+    sheet.cell(row, 2).value
+    #  sheet.cell(row, 10).value,
+    #  sheet.cell(row, 8).value
+    )
+
+print(invoiceList)
+
